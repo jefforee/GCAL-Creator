205ac8ebc2f80876da3fd93eeb614aea6b388499
Jeffrey Huang <jeffreyhuang2002@gmail.com>
2023-12-25 00:29:39 -0800

    Integrated ability to create an event with provided details including startDate, endDate, description, location, and event title

--- popup.js
+++ popup.js
@@ -1,15 +1,12 @@
-const openAIapiKey = "None";
-const openAIapiUrl = "https://api.openai.com/v1/completions";
-const googleapiKey = "None";
-const googleClientID = "944690984731-oadlt5ppa7j3n6ifb1iq00mnke2ma463.apps.googleusercontent.com";
+
 
 const processInput = async () => {
   let userInput = document.getElementById("userInput").value;
-  let prompt = `What is 5 + ${userInput}`;
+  let prompt = `Given the prompt '${userInput}', find the start date, endDate, event title, location, and create a short description for the event without all the logistical data. For the date format it like YYYY-MM-DDTHH:mm:ss-08:00. Comma separate the values.`;
   console.log("Calling GPT3");
 
   try {
-    const response = await fetch(apiUrl, {
+    const response = await fetch(openAIapiUrl, {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
@@ -85,44 +82,105 @@ const processInput = async () => {
 //   // Load the API client and auth2 library
 // gapi.load('client:auth2', initClient);
   
-document.getElementById('createEvent').addEventListener('click', createEvent);
+// document.getElementById('createEvent').addEventListener('click', createEvent);
 
-function createEvent() {
-  chrome.identity.getAuthToken({ interactive: true }, function (token) {
-    if (chrome.runtime.lastError || !token) {
-      console.error('Error getting authorization token:', chrome.runtime.lastError);
-      return;
-    }
+// function createEvent() {
+//   chrome.identity.getAuthToken({ interactive: true }, function (token) {
+//     if (chrome.runtime.lastError || !token) {
+//       console.error('Error getting authorization token:', chrome.runtime.lastError);
+//       return;
+//     }
 
-    const now = new Date();
-    const oneHourLater = new Date(now.getTime() + 60 * 60 * 1000);
+//     const now = new Date();
+//     const oneHourLater = new Date(now.getTime() + 60 * 60 * 1000);
 
-    const event = {
-      'summary': 'Event Title',
-      'description': 'Event Description',
-      'start': {
-        'dateTime': now.toISOString(),
-        'timeZone': 'UTC',
-      },
-      'end': {
-        'dateTime': oneHourLater.toISOString(),
-        'timeZone': 'UTC',
-      },
-    };
+//     const event = {
+//       'summary': 'Event Title',
+//       'description': 'Event Description',
+//       'start': {
+//         'dateTime': now.toISOString(),
+//         'timeZone': 'UTC',
+//       },
+//       'end': {
+//         'dateTime': oneHourLater.toISOString(),
+//         'timeZone': 'UTC',
+//       },
+//     };
 
-    fetch('https://www.googleapis.com/calendar/v3/calendars/primary/events', {
-      method: 'POST',
-      headers: {
-        'Authorization': 'Bearer ' + token,
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify(event),
-    })
-    .then(response => response.json())
-    .then(data => console.log('Event created:', data))
-    .catch(error => console.error('Error creating event:', error));
-  });
+//     fetch('https://www.googleapis.com/calendar/v3/calendars/primary/events', {
+//       method: 'POST',
+//       headers: {
+//         'Authorization': 'Bearer ' + token,
+//         'Content-Type': 'application/json',
+//       },
+//       body: JSON.stringify(event),
+//     })
+//     .then(response => response.json())
+//     .then(data => console.log('Event created:', data))
+//     .catch(error => console.error('Error creating event:', error));
+//   });
+// }
+
+document.getElementById('createEvent').addEventListener('click', openCalendarPopup);
+
+function openCalendarPopup() {
+    const startDate = new Date('2023-12-25T13:00:00-08:00');
+    const endDate = new Date('2023-12-25T14:00:00-08:00');
+    const title = 'Christmas Event';
+    const location = 'Your Location';
+    const description = 'Celebrate Christmas!';
+
+    const formattedStartDate = startDate.toISOString().replace(/-|:|\.\d+/g, '');
+    const formattedEndDate = endDate.toISOString().replace(/-|:|\.\d+/g, '');
+
+    const url = `https://calendar.google.com/calendar/r/eventedit?dates=${formattedStartDate}/${formattedEndDate}&text=${encodeURIComponent(title)}&location=${encodeURIComponent(location)}&details=${encodeURIComponent(description)}`;
+
+    chrome.tabs.create({ url: url });
 }
+
+
+
+
+
+
+// chrome.identity.getAuthToken({ 'interactive': false }, function(token) {
+//     console.log(token);
+
+//     const event = {
+//         'summary': 'Event Title',
+//         'description': 'Event Description',
+//         'start': {
+//             'dateTime': '2024-01-01T10:00:00',
+//             'timeZone': 'UTC',
+//         },
+//         'end': {
+//             'dateTime': '2024-01-01T12:00:00',
+//             'timeZone': 'UTC',
+//         },
+//     };
+
+//     const headers = new Headers({
+//         'Authorization': 'Bearer ' + token,
+//         'Content-Type': 'application/json',
+//     });
+
+//     const queryParams = {
+//         method: 'POST',
+//         headers: headers,
+//         body: JSON.stringify(event),
+//     };
+
+//     fetch('https://www.googleapis.com/calendar/v3/calendars/primary/events', queryParams)
+//         .then(response => response.json())
+//         .then(function(data) {
+//             console.log('Event created:', data);
+//             // Do whatever you need with the data!
+//         })
+//         .catch(error => {
+//             console.error('Error creating event:', error);
+//         });
+// });
+
 
 
 document.addEventListener("DOMContentLoaded", function () {
