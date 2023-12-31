3be3967be7448184a930b072e00fafb4c5119e26
Jeffrey Huang <jeffreyhuang2002@gmail.com>
2023-12-31 15:26:06 -0800

    Changed from davinci-text-003 to gpt-3.5-turbo model

--- popup.js
+++ popup.js
@@ -1,13 +1,13 @@
 // All necessary information
-const openAIapiKey = "";
-const openAIapiUrl = "https://api.openai.com/v1/completions";
-const googleapiKey = "";
-const googleClientID = "";
+const openAIapiKey = "FILL";
+const openAIapiUrl = "https://api.openai.com/v1/chat/completions";
+const googleapiKey = "FILL";
+const googleClientID = "FILL";
 
 // Provide the input to OpenAI and process it. The AI returns a response that formats the data for createCalendarEvent function.
 const processInput = async () => {
   let userInput = document.getElementById("userInput").value;
-  let prompt = `Given the prompt '${userInput}', find the start date, endDate, event title, location, and create a short description for the event without all the logistical data. For the date format it like YYYY-MM-DDTHH:mm:ss-08:00. Comma separate the values. Don't include what they are.`;
+  let prompt = `Given the prompt ${userInput}, find the start date, endDate, event title, location, and create a short and fun description for the event . Format the date as YYYY-MM-DDTHH:mm:ss-08:00. Format the response as comma separate the values. Default the year to 2024 if not specified. Default the endDate to be startDate plus 2 hours if not specified or else write 'N/A'. Assume dates are given my 'month/day'. `;
   console.log("Calling GPT3");
 
   try {
@@ -18,10 +18,12 @@ const processInput = async () => {
         Authorization: `Bearer ${openAIapiKey}`,
       },
       body: JSON.stringify({
-        model: "text-davinci-003",
-        prompt: prompt,
-        max_tokens: 100,
-        temperature: 1,
+        model: "gpt-3.5-turbo",
+        messages: [{role: "user", content: prompt}],
+        max_tokens: 2048,
+        temperature: 0.2,
+        n: 1,
+        stop: null
       }),
     });
 
@@ -30,7 +32,7 @@ const processInput = async () => {
     }
 
     const data = await response.json();
-    const completedText = data.choices[0].text;
+    const completedText = data.choices[0].message.content.trim();
 
     createCalendarEvent(completedText);
   } catch (error) {
@@ -40,12 +42,19 @@ const processInput = async () => {
 
 // Takes in event details, formatted by OpenAI API, and then provides the data into url to create a Google Calendar event, allowing option for user to continue editing.
 function createCalendarEvent(eventDetails) {
+    console.log(eventDetails);
     // eventDetails is returned as CSV as advised by prompt. Use split to get all the data.
     const dataArray = eventDetails.split(',');
     const fixedDate = dataArray[0].replace(/\n/g, '')
 
+    if (fixedDate == "N/A" || dataArray[1] == "N/A") {
+        alert("Provide a date please.")
+        return
+    }
+    console.log(fixedDate); 
+    console.log(dataArray[1]);
     const startDate = new Date(fixedDate);
-    const endDate = new Date(dataArray[1]);
+    const endDate = new Date(dataArray[1].trim());
     const title = dataArray[2];
     const location = dataArray[3];
     const description = dataArray[4];
